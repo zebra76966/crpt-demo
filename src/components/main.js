3fed0af2ad7dcd8218f84e52cc955405a4c7e573
zebra <rs288442@gmail.com>
2023-05-28 00:09:54 +0530

    Added multi-filters

--- src/components/main.js
+++ src/components/main.js
@@ -27,10 +27,13 @@ const Main = () => {
                 </p>
               </div>
             </div>
-            <div className="row mx-0">
-              <div className="col-9 pe-1">
-                <div className="p-3 pe-0">
-                  <div className="w-100  rounded-3 p-2" style={{ backgroundColor: "#1b1b1e" }}>
+            <div className="px-3">
+              <div
+                className="row mx-0 p-3 ps-0 pe-0 rounded"
+                style={{ backgroundColor: "#1b1b1e" }}
+              >
+                <div className="col-9 d-flex">
+                  <div className="w-100   p-2">
                     <div className="togglers w-100 ">
                       <div
                         className="cToggles d-flex border border-1 border-light rounded-pill p-2"
@@ -57,54 +60,60 @@ const Main = () => {
 
                     <Lchart cdata={togglers} cType={chartType} />
                   </div>
+                  <div className="h-100 opacity-25" style={{ background: "rgb(183, 183, 185)" }}>
+                    <div className="vr"></div>
+                  </div>
                 </div>
-              </div>
-              <div className="col-3 ps-1">
-                <div className=" p-3 ps-0">
-                  <div className="w-100 rounded-3 p-2" style={{ backgroundColor: "#1b1b1e" }}>
-                    <p className="text-light opacity-75">Market Sectors</p>
-                    <div className="listDash">
-                      <div className="form-check form-switch mb-3 ">
-                        <input
-                          className="form-check-input"
-                          type="checkbox"
-                          role="switch"
-                          id="flexSwitchCheckChecked"
-                          checked={togglers.length == 0 ? true : false}
-                          onChange={() => setToogler([])}
-                        />
-                        <label className="form-check-label text-light" for="flexSwitchCheckChecked">
-                          All
-                        </label>
+                <div className="col-3 ps-1">
+                  <div className=" p-3 ps-0">
+                    <div className="w-100 rounded-3 p-2" style={{ backgroundColor: "#1b1b1e" }}>
+                      <p className="text-light opacity-75">Market Sectors</p>
+                      <div className="listDash">
+                        <div className="form-check form-switch mb-3 ">
+                          <input
+                            className="form-check-input"
+                            type="checkbox"
+                            role="switch"
+                            id="flexSwitchCheckChecked"
+                            checked={togglers.length == 0 ? true : false}
+                            onChange={() => setToogler([])}
+                          />
+                          <label
+                            className="form-check-label text-light"
+                            for="flexSwitchCheckChecked"
+                          >
+                            All
+                          </label>
+                        </div>
+                        {coindata.map((ini, i) => {
+                          return (
+                            <div className="form-check form-switch mb-3 ">
+                              <input
+                                className="form-check-input"
+                                type="checkbox"
+                                role="switch"
+                                id="flexSwitchCheckChecked"
+                                checked={togglers.includes(i) ? true : false}
+                                onChange={() => {
+                                  togglers.includes(i)
+                                    ? setToogler(() => {
+                                        const updatedTogglers = [...togglers];
+                                        updatedTogglers.splice(togglers.indexOf(i), 1);
+                                        return updatedTogglers;
+                                      })
+                                    : setToogler([...togglers, i]);
+                                }}
+                              />
+                              <label
+                                className="form-check-label text-light text-truncate w-100"
+                                for="flexSwitchCheckChecked"
+                              >
+                                {ini.currency}
+                              </label>
+                            </div>
+                          );
+                        })}
                       </div>
-                      {coindata.map((ini, i) => {
-                        return (
-                          <div className="form-check form-switch mb-3 ">
-                            <input
-                              className="form-check-input"
-                              type="checkbox"
-                              role="switch"
-                              id="flexSwitchCheckChecked"
-                              checked={togglers.includes(i) ? true : false}
-                              onChange={() => {
-                                togglers.includes(i)
-                                  ? setToogler(() => {
-                                      const updatedTogglers = [...togglers];
-                                      updatedTogglers.splice(togglers.indexOf(i), 1);
-                                      return updatedTogglers;
-                                    })
-                                  : setToogler([...togglers, i]);
-                              }}
-                            />
-                            <label
-                              className="form-check-label text-light text-truncate w-100"
-                              for="flexSwitchCheckChecked"
-                            >
-                              {ini.currency}
-                            </label>
-                          </div>
-                        );
-                      })}
                     </div>
                   </div>
                 </div>
