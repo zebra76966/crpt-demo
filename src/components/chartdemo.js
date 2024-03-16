309aeb20703a68c87bfd3f85ee9cdac9088a33bd
zebra <rs288442@gmail.com>
2024-03-17 00:49:00 +0530

    Added radar data

--- src/components/chartdemo.js
+++ src/components/chartdemo.js
@@ -31,14 +31,29 @@ const Lchart = (props) => {
   };
 
   const data2 = {
-    labels: ["Thing 1", "Thing 2", "Thing 3", "Thing 4", "Thing 5", "Thing 6"],
+    labels: ["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"],
     datasets: [
       {
-        label: "# of Votes",
-        data: [2, 9, 3, 5, 2, 3],
-        backgroundColor: "rgba(66, 158, 245, 0.7)",
-        borderColor: "rgba(255, 255, 255)",
-        borderWidth: 1,
+        label: "My First Dataset",
+        data: [65, 59, 90, 81, 56, 55, 40],
+        fill: true,
+        backgroundColor: "rgba(255, 99, 132, 0.2)",
+        borderColor: "rgb(255, 99, 132)",
+        pointBackgroundColor: "rgb(255, 99, 132)",
+        pointBorderColor: "#fff",
+        pointHoverBackgroundColor: "#fff",
+        pointHoverBorderColor: "rgb(255, 99, 132)",
+      },
+      {
+        label: "My Second Dataset",
+        data: [28, 48, 40, 19, 96, 27, 100],
+        fill: true,
+        backgroundColor: "rgba(54, 162, 235, 0.2)",
+        borderColor: "rgb(54, 162, 235)",
+        pointBackgroundColor: "rgb(54, 162, 235)",
+        pointBorderColor: "#fff",
+        pointHoverBackgroundColor: "#fff",
+        pointHoverBorderColor: "rgb(54, 162, 235)",
       },
     ],
   };
